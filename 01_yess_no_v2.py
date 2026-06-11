0acaac1ff38d75a71e8e57c79adf07ef79f4f5b2
Hans Du <2020426369a@gmail.com>
2026-06-12 11:25:07 +1200

    v2

--- 01_yess_no_v2.py
+++ 01_yess_no_v2.py
@@ -9,11 +9,12 @@ def yes_no(question):
             print("Please enter yes or no.")
 
 
+while True:
+    want_instructions = yes_no("Do you want to read the instructions? ")
 
-want_instructions = yes_no("Do you want to read the instructions? ")
 
+    if want_instructions=="yes":
+        print("Instruction go here.")
 
-if want_instructions:
-    print("Instruction go here.")
-
-print("We are done.")
+    print("program continues...")
+    print()
