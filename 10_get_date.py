09255047a915f31d44e5deb2800d9b3d039c6212
Hans Du <2020426369a@gmail.com>
2026-06-27 20:24:50 +1200

    get date and export

---
+++ 10_get_date.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+today = date.today()
+day=today.strftime("%d")
+month=today.strftime("%m")
+year=today.strftime("%Y")
+
+heading = "The current date is: {}/{}/{}".format(day,month,year)
+filename = "MMF_{}_{}_{}".format(year,month,day)
+
+print(heading)
+print("The filename will be: {}.txt".format(filename))
