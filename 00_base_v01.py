e4ab18ecace8e99d7369bfaf81de7114272278e2
Hans Du <2020426369a@gmail.com>
2026-06-27 19:18:04 +1200

    add the function into base 01

--- 00_base_v01.py
+++ 00_base_v01.py
@@ -1,3 +1,4 @@
+import pandas
 def yes_no(question):
     while True:
         response = input(question).lower()
@@ -51,12 +52,26 @@ def string_checker(question, num_letters, valid_responses):
                 return item
         print(error)
 
+def currency(x):
+    return "${:.2f}".format(x)
 
-Max_ticket=3
+
+
+Max_ticket=5
 ticket_sold=0
 
 yes_no_list = ["yes","no"]
 payment_list = ["cash","credit"]
+
+all_names=[]
+all_tickets_costs=[]
+all_surcharges=[]
+
+mini_movie_fundrais_dict = {
+    "Name":all_names,
+    "Ticket Price":all_tickets_costs,
+    "Surcharge":all_surcharges
+}
 
 want_instructions = string_checker("Do you want to read the instructions? (y/n)",1,yes_no_list)
 
@@ -88,10 +103,40 @@ while ticket_sold<Max_ticket:
     
     ticket_cost = calc_ticket_price(age)
 
-    payment_method = string_checker("Please choose a payment method (cash/credit): ",2,payment_list)
-    print("Age: {}, ticket price: ${:.2f}".format(age, ticket_cost))  
+    pay_method = string_checker("Please choose a payment method (cash/credit): ",2,payment_list)
+    if pay_method == "cash":
+        surcharge = 0
+    else:
+        surcharge = ticket_cost * 0.05
     
     ticket_sold+=1
+    
+    all_names.append(name)
+    all_tickets_costs.append(ticket_cost)
+    all_surcharges.append(surcharge)
+
+
+mini_movie_frame=pandas.DataFrame(mini_movie_fundrais_dict)
+mini_movie_frame=mini_movie_frame.set_index("Name")
+
+mini_movie_frame["Total"] = mini_movie_frame["Surcharge"] \
+                            + mini_movie_frame["Ticket Price"]
+
+mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
+
+total = mini_movie_frame['Total'].sum()
+profit = mini_movie_frame['Profit'].sum()
+
+add_dollars = ['Ticket Price','Surcharge','Total','Profit']
+for var_item in add_dollars:
+    mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
+
+print("---Ticket Data---")
+print()
+print(mini_movie_frame)
+print()
+print("Total Ticket Sales: ${:.2f}".format(total))
+print("Total Profit: ${:.2f}".format(profit))
     
 if ticket_sold==Max_ticket:
     print("Congratulations! You have sold all the tickets!")
--- 08_ticket_cost.py
+++ 08_ticket_cost.py
@@ -1,5 +1,8 @@
 import pandas
 
+def currency(x):
+    return "${:.2f}".format(x)
+
 all_names=["a","b","c","d","e"]
 all_tickets_costs=[7.50,7.50,10.50,10.50,6.50]
 surcharges=[0,0,0.53,0.53,0]
@@ -11,6 +14,7 @@ mini_movie_fundrais_dict = {
 }
 
 mini_movie_frame=pandas.DataFrame(mini_movie_fundrais_dict)
+mini_movie_frame=mini_movie_frame.set_index("Name")
 
 mini_movie_frame["Total"] = mini_movie_frame["Surcharge"] \
                             + mini_movie_frame["Ticket Price"]
@@ -19,6 +23,14 @@ mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
 
 total = mini_movie_frame['Total'].sum()
 profit = mini_movie_frame['Profit'].sum()
+
+add_dollars = ['Ticket Price','Surcharge','Total','Profit']
+for var_item in add_dollars:
+    mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
+
+print("---Ticket Data---")
+print()
 print(mini_movie_frame)
+print()
 print("Total Ticket Sales: ${:.2f}".format(total))
 print("Total Profit: ${:.2f}".format(profit))
